5771fb6c66a058c7693a7c0d9fe3f049facc1ce7
draakuns <draakuns>
2018-09-25 00:44:42 +0200

    more download scrips

---
+++ download_mod.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+### $1 being the workshop item to download
+
+. cfg
+
+fn_steamcmd_dl(){
+#set -x
+#${STEAM_EXE} +login ${STEAMUSR} ${STEAMPWD} +force_install_dir ${MOD_DIR} +"workshop_download_item 107410 $1" validate +quit
+${STEAM_EXE} +login dragoonspain +force_install_dir ${MOD_DIR} +"workshop_download_item 107410 $1" validate +quit
+}
+
+fn_steamcmd_status(){
+${STEAM_EXE} +login ${STEAMUSR} ${STEAMPWD} +force_install_dir ${MOD_DIR} +"workshop_status $1" +quit
+}
+
+#Main
+fn_steamcmd_dl "$1"
+#fn_steamcmd_status "$WS_ARMAID"
+#fn_steamcmd_status "$WS_ARMADSID"
